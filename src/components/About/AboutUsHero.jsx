828c1f656b642d22292c53ea23d2bbcbd59a7bfe
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2024-09-13 01:39:04 +0500

    added logo in the background of home and about page hero section

--- src/components/About/AboutUsHero.jsx
+++ src/components/About/AboutUsHero.jsx
@@ -1,5 +1,6 @@
 import { useScroll, useTransform, motion } from "framer-motion";
 import React, { useRef } from "react";
+import { useInView } from "react-intersection-observer";
 
 const AboutUsHero = () => {
   const SectionRef = useRef(null);
@@ -8,6 +9,8 @@ const AboutUsHero = () => {
     offset: ["start end", "end start"],
   });
   const y = useTransform(scrollYProgress, [0, 1], ["-20%", "10%"]);
+
+  const [ref, inView] = useInView({ triggerOnce: true });
 
   return (
     <main
@@ -26,14 +29,26 @@ const AboutUsHero = () => {
         className="absolute w-full h-[120%] -z-10"
         style={{ top: y }}
       ></motion.div>
-      <h1 className="text-5xl md:text-5xl lg:text-[89px] xl:text-[109.7px] 2xl:text-[129px] lg:leading-[96px] 2xl:leading-[136px] font-bold tracking-tight uppercase quantico-fonts z-10">
+      <motion.h1
+        ref={ref}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 50 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="text-5xl md:text-5xl lg:text-[89px] xl:text-[109.7px] 2xl:text-[129px] lg:leading-[96px] 2xl:leading-[136px] font-bold tracking-tight uppercase quantico-fonts z-10"
+      >
         who we are
-      </h1>
-      <p className="lg:ml-3 text-[17px] 2xl:text-xl tracking-wide lg:w-2/3 z-10">
+      </motion.h1>
+      <motion.p
+        ref={ref}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 50 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="lg:ml-3 text-[17px] 2xl:text-xl tracking-wide lg:w-2/3 z-10"
+      >
         Bravo Foxtrot is a leading software company specializing in innovative
         and practical software solutions that incorporate AI and ML to combat
         criminal and illicit activities.
-      </p>
+      </motion.p>
     </main>
   );
 };
--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,6 +1,9 @@
+import { motion } from "framer-motion";
 import React from "react";
+import { useInView } from "react-intersection-observer";
 
 const Hero = () => {
+  const [ref, inView] = useInView({ triggerOnce: true });
   return (
     <div className="w-full horizontal-padding h-screen flex flex-col items-start justify-center gap-6 2xl:gap-12 text-white relative">
       {/* <video autoPlay muted loop className="w-full h-full absolute inset-0 z-0">
@@ -12,12 +15,24 @@ const Hero = () => {
         className="w-auto h-[90%] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0 opacity-40"
       />
 
-      <h1 className="text-4xl md:text-5xl lg:text-[89px] xl:text-[99.7px] 2xl:text-[129px] lg:leading-[116px] 2xl:leading-[136px] font-bold tracking-tight uppercase quantico-fonts z-10">
+      <motion.h1
+        ref={ref}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 50 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="text-4xl md:text-5xl lg:text-[89px] xl:text-[99.7px] 2xl:text-[129px] lg:leading-[116px] 2xl:leading-[136px] font-bold tracking-tight uppercase quantico-fonts z-10"
+      >
         Redefining Safety Through Innovation
-      </h1>
-      <p className="lg:ml-3 text-2xl 2xl:text-4xl tracking-wide z-10">
+      </motion.h1>
+      <motion.p
+        ref={ref}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 50 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="lg:ml-3 text-2xl 2xl:text-4xl tracking-wide z-10"
+      >
         Courage. Commitment. Justice.
-      </p>
+      </motion.p>
     </div>
   );
 };
